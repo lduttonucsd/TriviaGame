177f39f70917b2a926f8132ed4653e98493dbbb2
lduttonucsd <47542535+lduttonucsd@users.noreply.github.com>
2019-03-03 01:44:00 -0800

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+// click start button to show questions
+// window.onload = function () {
+//     $("#startButton").on("click")
+// $("#display").text("00:00");
+    
+
+
+
+
+
+
+
+
+   
+
+// var timerInterval;
+// var timerRunning = false;
+// var time = 0;
+// $("#beginTrivia").on("click")
+    
+
+    
+
+
+
+// timer counts down from 120-0
+// function start() {}
+    // if (!clockRunning) {
+    //     intervalId = setInterval(count, 120000);
+    //     clockRunning = true;}
+
+// and start timer
+// $("#beginTrivia").on("click", start);
+
+
+
+// user clicks done to end trivia
+
+// timer stops
+
+// correct answers tallied
+
+// incorrect answers tallied
+
+// BONUS show percentage correct?
+
